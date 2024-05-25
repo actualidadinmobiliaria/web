30a64533eda61bc7cf05fd16be58df9f15d3ff0a
actualidadinmobiliaria <inmobiliariaaactualidad@gmail.com>
2024-05-25 15:42:30 -0500

    colocando un contador de visitas con javascript y html

--- scripts.js
+++ scripts.js
@@ -318,3 +318,40 @@ const menuEnlaces = document.querySelector(".menu-enlaces");
 menuIcono.addEventListener("click", () => {
   menuEnlaces.classList.toggle("active");
 });
+
+
+
+
+function increaseCounter() {
+  var visitCount = getCookie('visitCount');
+  if (visitCount) {
+    visitCount = parseInt(visitCount) + 1;
+  } else {
+    visitCount = 1;
+  }
+  setCookie('visitCount', visitCount, 365);
+  document.getElementById('visitCounter').innerText = visitCount;
+}
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
